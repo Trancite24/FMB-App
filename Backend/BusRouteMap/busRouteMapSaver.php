3fec6e0ca2c5fb3c28704a846d59ceb272581216
Keet Malin Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2016-12-07 21:35:46 +0530

    BAckend for the Admin Panel for adding map routes completed

--- Backend/BusRouteMap/busRouteMapSaver.php
+++ Backend/BusRouteMap/busRouteMapSaver.php
@@ -10,10 +10,46 @@
 
 include '../Connection/connection.php';
 
-$data = $_REQUEST['mapdata'];
+$route_map = $_REQUEST['route_map'];
+$start_lat = $_REQUEST['start_lat'];
+$start_long = $_REQUEST['start_long'];
+$start = $_REQUEST['start'];
+$destination = $_REQUEST['destination'];
 $route_no = $_REQUEST['route_no'];
+$dest_lat= $_REQUEST['dest_lat'];
+$dest_long = $_REQUEST['dest_long'];
 
-//create query to update the database
-$query = "UPDATE bus_route SET route_map='$data' WHERE route_no = '$route_no'";
-//connect the query and retrieve the result
-$result = $conn->query($finalQuery);
+$route_no = 2;
+
+$query = "SELECT * FROM bus_route WHERE route_no=$route_no";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $query = "UPDATE bus_route SET route_map='$route_map', 
+                            start_lat='$start_lat',
+                            start_long='$start_long',
+                            start='$start',
+                            destination='$destination',
+                            dest_lat='$dest_lat',
+                            dest_long='$dest_long'
+              WHERE route_no = '$route_no'";
+    $result = $conn->query($query);
+    if ($result){
+        die("updated");
+    }
+    die("failed");
+} else {
+    $query = "INSERT INTO bus_route (route_map,start_lat, start_long , start,
+                          destination ,dest_lat , dest_long , route_no )
+                          VALUES 
+                            ('$route_map','$start_lat',
+                            '$start_long','$start',
+                            '$destination','$dest_lat',
+                            '$dest_long','$route_no')";
+
+    $result = $conn->query($query);
+    if ($result){
+        die("updated");
+    }
+    die("failed");
+}
