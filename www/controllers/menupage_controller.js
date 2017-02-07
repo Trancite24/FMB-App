add3e3504d898a4e8fa2c70131bc013a25279abc
Keet Malin Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2017-02-07 16:42:47 +0530

    added pages for about us and contact us

--- www/controllers/menupage_controller.js
+++ www/controllers/menupage_controller.js
@@ -3,6 +3,27 @@
  */
 
 function goAboutPage(){
+    window.location = "aboutus.html";
+}
+
+function goContactUsPage(){
     window.location = "contact.html";
 }
+
+function rateUs(){
+    window.location = "https://play.google.com/store/apps/details?id=com.titans.yung&hl=en";
+}
+
+function goFeaturesPage(){
+    window.location = "contact.html";
+}
+
+function back(){
+    window.history.back();
+}
+
+function goHome(){
+    window.history.back();
+}
+
 
