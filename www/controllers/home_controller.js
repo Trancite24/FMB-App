aedc1d9973c6537f7bfb1644c0b897b741870379
Keet Malin Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2017-02-28 11:18:36 +0530

    Code fixed in home.html

--- www/controllers/home_controller.js
+++ www/controllers/home_controller.js
@@ -80,3 +80,42 @@ function closeModal() {
     document.getElementById('modal').style.display = 'none';
 }
 
+function onload_autosugest(){
+    $(function () {
+
+        var autoSuggest = JSON.parse(localStorage.getItem("autosuggest"));
+
+
+        if(autoSuggest==null){
+            var autosuggest_adddresses = [];
+
+            jQuery.ajax({
+                type: "GET",
+                dataType: 'jsonp',
+                url: "http://www.titansmora.org/findmybusfinal/LocationsLIst/getLocationsList.php",
+                success: function (obj) {
+                    localStorage.setItem("autosuggest",JSON.stringify(obj));
+                    if (!('error' in obj)) {
+                        $.each(obj, function (index, element) {
+                            autosuggest_adddresses.push(element);
+                            $('#cities').append(
+                                '<option value=' + '"' + element + '"' + '></option>');
+                        });
+
+                    }
+                    else {
+                        console.log(obj.error);
+                    }
+                }
+            });
+        }else{
+            for (var i = 0; i < autoSuggest.length; i++) {
+                $('#cities').append(
+                    '<option value=' + '"' + autoSuggest[i] + '"' + '></option>');
+            }
+
+        }
+
+    });
+}
+
---
+++ www/js/cordova_plugins.js
@@ -0,0 +1,81 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/com.telerik.plugins.nativepagetransitions/www/NativePageTransitions.js",
+        "id": "com.telerik.plugins.nativepagetransitions.NativePageTransitions",
+        "clobbers": [
+            "window.plugins.nativepagetransitions"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-dialogs": "1.2.1",
+    "cordova-plugin-splashscreen": "3.1.0",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-app-event": "1.2.0",
+    "de.appplant.cordova.plugin.local-notification": "0.8.4",
+    "com.telerik.plugins.nativepagetransitions": "0.6.5",
+    "cordova-plugin-crosswalk-webview": "2.1.0"
+};
+// BOTTOM OF METADATA
+});
